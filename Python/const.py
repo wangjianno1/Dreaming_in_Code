9fc225f3a692778e693c0f3d4814905d01d9a00d
wangjianno1 <wangjianno1@sina.com>
2016-08-27 01:28:35 +0800

    add const.py

---
+++ Python/const.py
@@ -0,0 +1,34 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+file: autocheck_common.py
+data: 2016-08-25
+"""
+
+class AutoCheckResult(object):
+    """自动检查结果
+    """
+    PASS = 0 #检查通过
+    FAIL = 1 #检查失败
+    REDO = 2 #重新检查
+    ERR = 3  #接口异常, 不重新检查
+
+    names = ['PASS',
+             'FAIL',
+             'REDO',
+             'ERR'
+            ]
+    name_map = dict(zip(names, range(len(names))))
+
+def get_check_result():
+    return AutoCheckResult.FAIL
+
+if __name__ == '__main__':
+    check_retcode = get_check_result()
+    if check_retcode == AutoCheckResult.PASS:
+        print '检查通过!!'
+        # 使用AutoCheckResult.names[]来获取可读性好的结果
+        print 'check result: {}'.format(AutoCheckResult.names[check_retcode])
+    else:
+        print '检查异常!!'
+        print 'check result: {}'.format(AutoCheckResult.names[check_retcode])
--- Python/readme.txt
+++ Python/readme.txt
@@ -1,3 +1,5 @@
 - http_util.py        Http请求工具
 - logtool.py          日志打印工具
 - mon_info.py         监控采集脚本
+- const.py            常量定义
+
